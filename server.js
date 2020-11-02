c162ed6e177d46a211aedc89ca3f718b9cd36ef1
里屋純平 <satoya@MacBook-Pro.local>
2020-11-02 22:17:02 +0900

    JWT Authentication

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect Database
 connectDB();
 
+//Init Middleware
+app.use(express.json({ extended: true }))
+
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
