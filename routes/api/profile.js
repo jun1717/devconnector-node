859502c0e39110a1c12d508a3747b730a1c741f7
里屋純平 <satoya@MacBook-Pro.local>
2020-11-19 08:21:04 +0900

    Created App

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -139,8 +139,8 @@ router.get('/user/:user_id', async (req, res) => {
 // @access Private
 router.delete('/', auth, async (req, res) => {
   try {
-    // @todo - remove users posts
-
+    // Remove User posts
+    await this.post.deleteMany({ user: req.user.id })
     // Remove Profile
     await Profile.findOneAndRemove({ user: req.user.id })
 
